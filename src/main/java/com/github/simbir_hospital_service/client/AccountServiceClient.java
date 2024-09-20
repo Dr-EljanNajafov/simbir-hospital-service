3aa09eac10deddbdd59cf7eb14dcba6d6b830fab
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-20 14:00:09 +0400

    RequestHeader -> RequestParam

--- src/main/java/com/github/simbir_hospital_service/client/AccountServiceClient.java
+++ src/main/java/com/github/simbir_hospital_service/client/AccountServiceClient.java
@@ -5,11 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "account-service",  url = "${services.account-service.host}:${services.account-service.port}")
+@FeignClient(name = "account-service", url = "${services.account-service.host}:${services.account-service.port}")
 @Component
 public interface AccountServiceClient {
 
     @GetMapping("/api/Authentication/Validate")
-    ResponseEntity<?> validateToken(@RequestHeader("accessToken") String token);
+    ResponseEntity<?> validateToken(@RequestParam("accessToken") String token);
 }
